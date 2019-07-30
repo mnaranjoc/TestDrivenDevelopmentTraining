ef5153aa03a73ce4a23068c33826beb9031d8902
Manuel Naranjo <manuelnaranjo@outlook.com>
2019-07-30 17:49:42 -0500

    Chapter 1 excercises

---
+++ TDD/Chapter1/UtilitiesTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+namespace Chapter1
+{
+    [TestFixture]
+    public class UtilitiesTests
+    {
+        [TestCase]
+        public void ShouldFindOneYInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "y";
+            var expectedResult = 1;
+            var classUnderTest = new StringUtilities();
+            var actualResult =
+            classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void ShouldFindTwoSInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new StringUtilities();
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void SearchShouldBeCaseSenstive()
+        {
+            var stringToCheck = "mySterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new StringUtilities();
+            var actualResult =
+            classUnderTest.CountOccurences(stringToCheck,
+            stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
