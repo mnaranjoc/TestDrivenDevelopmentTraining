f2cd92a5d050a53b884f26bb04fb18e3b307d1c3
Manuel Naranjo <45862940+mnaranjoc@users.noreply.github.com>
2019-07-30 17:58:44 -0500

    Development (#1)
    * Chapter 1 excercises

---
+++ TDD/Chapter1/StringUtilities.cs
@@ -0,0 +1,24 @@
+﻿namespace Chapter1
+{
+    public class StringUtilities
+    {
+        public int CountOccurences(string stringToCheck,
+        string stringToFind)
+        {
+            if (stringToCheck == null || stringToCheck == "") return -1;
+            var stringAsCharArray = stringToCheck.ToUpper().ToCharArray();
+            var stringToCheckForAsChar = stringToFind.ToUpper().ToCharArray()[0];
+            var occuranceCount = 0;
+            for (var characterIndex = 0;
+                characterIndex <= stringAsCharArray.GetUpperBound(0);
+                characterIndex++)
+            {
+                if (stringAsCharArray[characterIndex] == stringToCheckForAsChar)
+                {
+                    occuranceCount++;
+                }
+            }
+            return occuranceCount;
+        }
+    }
+}
---
+++ TDD/Chapter1/UtilitiesTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+
+namespace Chapter1
+{
+    [TestFixture]
+    public class UtilitiesTests
+    {
+        [TestCase]
+        public void ShouldFindOneYInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "y";
+            var expectedResult = 1;
+            var classUnderTest = new StringUtilities();
+            var actualResult =
+            classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void ShouldFindTwoSInMysterious()
+        {
+            var stringToCheck = "mysterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new StringUtilities();
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void SearchShouldBeCaseSenstive()
+        {
+            var stringToCheck = "mySterious";
+            var stringToFind = "s";
+            var expectedResult = 2;
+            var classUnderTest = new StringUtilities();
+            var actualResult =
+            classUnderTest.CountOccurences(stringToCheck,
+            stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void ShouldBeAbleToHandleNulls()
+        {
+            string stringToCheck = null;
+            var stringToFind = "s";
+            var expectedResult = -1;
+            var classUnderTest = new StringUtilities();
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestCase]
+        public void ShouldBeAbleToHandleEmptyString()
+        {
+            string stringToCheck = "";
+            var stringToFind = "s";
+            var expectedResult = -1;
+            var classUnderTest = new StringUtilities();
+            var actualResult = classUnderTest.CountOccurences(stringToCheck, stringToFind);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
